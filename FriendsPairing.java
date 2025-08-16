41cdc00c709f15692cebc87f2a9a89ad44416bd5
Abhishek Kumar Singh <150012887+abhi17181@users.noreply.github.com>
2025-08-16 15:50:54 +0530

    Update FriendsPairing.java

--- FriendsPairing.java
+++ FriendsPairing.java
@@ -8,13 +8,14 @@ public class FriendsPairing {
         int pairWays=(n-1)* fnm2;
         int totalWays=fnm1+pairWays;
         return totalWays;
-        return friendsPairing(n-1)+(n-1)* friendsPairing(n-2);
+        //return friendsPairing(n-1)+(n-1)* friendsPairing(n-2);
 
     }
     public static void main(String[]args){
         System.out.println(friendsPairing(3));
     }
 }
+
 
 
 
